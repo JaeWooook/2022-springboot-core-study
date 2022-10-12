48df8bf20e28790298253f56a40ece069fce26b4
br12345678 <mystyle730@gmail.com>
2022-10-13 00:31:51 +0900

    스프링으로 전환하기 2

--- src/main/java/springboot/corestudy/MemberApp.java
+++ src/main/java/springboot/corestudy/MemberApp.java
@@ -1,15 +1,22 @@
 package springboot.corestudy;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import springboot.corestudy.member.Grade;
 import springboot.corestudy.member.Member;
 import springboot.corestudy.member.MemberService;
-import springboot.corestudy.member.MemberServiceImpl;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);//이게 스프링의 모든 빈을 생성해주는 것이다.
+        //어노테이션을 기반으로 Config를 하고있다. AppConfig에 있는 환경설정 정보를 가지고 스프링이 내부에서 @Bean들을 컨테이너에 객체 생성한것을 넣고 관리해준다.
+        MemberService memberService =  applicationContext.getBean("memberService", MemberService.class);//Config에서 메서드이름을 가진 객체를 찾는 메서드
+        //이름은 memberService이고 , 타입은 MemberService이다.
+
         //회원 가입 로직
 //        MemberService memberService = new MemberServiceImpl();
         Member member = new Member(1L, "memberA", Grade.VIP);//Long타입은 뒤에 L을 붙여야한다.
--- src/main/java/springboot/corestudy/OrderApp.java
+++ src/main/java/springboot/corestudy/OrderApp.java
@@ -1,23 +1,27 @@
 package springboot.corestudy;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import springboot.corestudy.member.Grade;
 import springboot.corestudy.member.Member;
 import springboot.corestudy.member.MemberService;
-import springboot.corestudy.member.MemberServiceImpl;
 import springboot.corestudy.order.Order;
 import springboot.corestudy.order.OrderService;
-import springboot.corestudy.order.OrderServiceImpl;
 
 public class OrderApp {
 
     public static void main(String[] args) {
 
-        AppConfig appConfig = new AppConfig();
-
-        MemberService memberService = appConfig.memberService(); // MemberServiceImpl 객체를 반환한다.
-        OrderService orderService = appConfig.orderService(); // OrderServiceImpl 객체를 반환한다.
+//        AppConfig appConfig = new AppConfig();
+//
+//        MemberService memberService = appConfig.memberService(); // MemberServiceImpl 객체를 반환한다.
+//        OrderService orderService = appConfig.orderService(); // OrderServiceImpl 객체를 반환한다.
 //        MemberService memberService = new MemberServiceImpl(null);
 //        OrderService orderService = new OrderServiceImpl(null, null);
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
         Member member  = new Member(memberId, "memberA", Grade.VIP);
