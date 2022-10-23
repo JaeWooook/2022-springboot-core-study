ef80150f689e9c2ef04812569ada013fae4889a1
br12345678 <mystyle730@gmail.com>
2022-10-24 00:15:13 +0900

    웹 애플리케이션과 싱글톤

--- src/test/java/springboot/corestudy/beandefinition/BeanDefinitionTest.java
+++ src/test/java/springboot/corestudy/beandefinition/BeanDefinitionTest.java
@@ -11,6 +11,7 @@ public class BeanDefinitionTest {
 
     AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class); // Java코드로 AppConfig로 넣는것은 앱컨피그의 팩토리메서드를 통해서 빈을 넣어주는 방식
     //이걸로 하면 팩토리빈네임이랑 팩토리메서드네임도 나온다. 팩토리빈을 통해서 등록이되는 방식이기 때문
+    //일반적인 방식은 팩토리빈을 통해서 빈을 등록한다.
     
 //    GenericXmlApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml"); //xml 형식으로 Bean 정의를 보는 방법
 
