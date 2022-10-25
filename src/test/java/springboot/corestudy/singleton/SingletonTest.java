1edb9bddb279ff9f3453cf265f954348d2638ec4
br12345678 <mystyle730@gmail.com>
2022-10-26 00:37:39 +0900

    싱글톤 컨테이너

--- src/test/java/springboot/corestudy/singleton/SingletonTest.java
+++ src/test/java/springboot/corestudy/singleton/SingletonTest.java
@@ -2,6 +2,8 @@ package springboot.corestudy.singleton;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import springboot.corestudy.AppConfig;
 import springboot.corestudy.member.MemberService;
 
@@ -42,5 +44,27 @@ public class SingletonTest {
         //singletonService1 == singletonService2
         assertThat(singletonService1).isSameAs(singletonService2);
         //isEqualTo는 자바의 equals비교 , isSameAs 자바의 == 비교이다.
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer() {
+
+        //스프링은 각기 다른 고객의 요청에 대해 호출된 객체들이 모두 동일한 인스턴스인 memberService를 반환하게 된다.
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        //스프링 컨테이너에서 생성한 것 이지만 싱글톤으로 알아서 해주기 때문에 2번 객체를 호출해도 같은 객체를 가진다.
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        //참조값이 같은 것을 확인
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        //memberService1 != memberService2
+//        assertThat(memberService1).isNotSameAs(memberService2);
+
+        //memberService1 == memberService2
+        assertThat(memberService1).isSameAs(memberService2);
     }
 }
