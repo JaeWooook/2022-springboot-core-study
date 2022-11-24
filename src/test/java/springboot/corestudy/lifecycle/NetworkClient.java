ee2adbdc576c8cea883f31d57951f42c164284f3
br12345678 <mystyle730@gmail.com>
2022-11-25 00:05:11 +0900

    인터페이스 InitializingBean, DisposableBean

--- src/test/java/springboot/corestudy/lifecycle/NetworkClient.java
+++ src/test/java/springboot/corestudy/lifecycle/NetworkClient.java
@@ -1,6 +1,9 @@
 package springboot.corestudy.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean; //destroy가 들어있다.
+import org.springframework.beans.factory.InitializingBean; //프로퍼티 셋팅이 끝나면 호출해주는것이다. 의존관계 주입이 끝나면 알려준다.
+
+public class NetworkClient implements InitializingBean, DisposableBean {
 
     private String url;
 
@@ -26,5 +29,19 @@ public class NetworkClient {
     //서비스 종료시 호출
     public void disconnect() {
         System.out.println("close: " + url);
+    }
+
+    //스프링에 의존적인 인터페이스라는게 단점이다. 내가 코드를 고칠수 없는 외부라이브러리가 들어있다. 요즘은 거의 사용하지 않는다.
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("NetworkClient.destroy");
+        disconnect();
     }
 }
