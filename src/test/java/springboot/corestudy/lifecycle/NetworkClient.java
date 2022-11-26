152cb1e864cf8e9fc9e21fe84b6b7546870f678e
br12345678 <mystyle730@gmail.com>
2022-11-26 12:29:38 +0900

    빈 등록 초기화, 소멸 메서드

--- src/test/java/springboot/corestudy/lifecycle/NetworkClient.java
+++ src/test/java/springboot/corestudy/lifecycle/NetworkClient.java
@@ -3,7 +3,7 @@ package springboot.corestudy.lifecycle;
 import org.springframework.beans.factory.DisposableBean; //destroy가 들어있다.
 import org.springframework.beans.factory.InitializingBean; //프로퍼티 셋팅이 끝나면 호출해주는것이다. 의존관계 주입이 끝나면 알려준다.
 
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient {
 
     private String url;
 
@@ -32,15 +32,15 @@ public class NetworkClient implements InitializingBean, DisposableBean {
     }
 
     //스프링에 의존적인 인터페이스라는게 단점이다. 내가 코드를 고칠수 없는 외부라이브러리가 들어있다. 요즘은 거의 사용하지 않는다.
-    @Override
-    public void afterPropertiesSet() throws Exception {
+//    @Override
+    public void init() {
         System.out.println("NetworkClient.afterPropertiesSet");
         connect();
         call("초기화 연결 메시지");
     }
 
-    @Override
-    public void destroy() throws Exception {
+//    @Override
+    public void close() {
         System.out.println("NetworkClient.destroy");
         disconnect();
     }
